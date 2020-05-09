4f541c4c769a2d608dc085e9692f24158b37ecce
geekJuneLin <junelin0216@gmail.com>
2020-05-09 15:07:16 +1200

    deploy mern stack project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const items = require("./routes/api/items");
 
@@ -25,6 +26,16 @@ mongoose
 
 // Use routes
 app.use("/api/items", items);
+
+// Serve static assets if in production model
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // Port num
 const PORT = process.env.PORT || 5000;
