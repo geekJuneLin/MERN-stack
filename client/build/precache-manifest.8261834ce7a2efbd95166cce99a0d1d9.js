3151bdbe929aaacd49a6d8800b24d8ee0e272a4a
geekJuneLin <junelin0216@gmail.com>
2020-05-10 18:17:30 +1200

    manually build the production

---
+++ client/build/precache-manifest.8261834ce7a2efbd95166cce99a0d1d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345c1d2a81c9a4bf00a006830d298ebf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f9583c25b431a042ebc",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "14b681aa0e853d092aa2",
+    "url": "/static/css/main.d17bc1af.chunk.css"
+  },
+  {
+    "revision": "8f9583c25b431a042ebc",
+    "url": "/static/js/2.678dc102.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.678dc102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b681aa0e853d092aa2",
+    "url": "/static/js/main.28d2859c.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
